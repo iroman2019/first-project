6357ca345b825c3533b17a116844a88d41d2a063
iroman2019 <roman.ildiko@pontsystems.eu>
2019-11-06 19:24:57 +0100

    Első autom commitom

--- szamologep.py
+++ szamologep.py
@@ -4,6 +4,13 @@ from selenium.webdriver.common.by import By
 driver = webdriver.Chrome()
 driver.get("https://iroman2019.github.io/first-project/")
 
-driver.find_element(By.NAME,"a") .send_keys("3")
-driver.find_element(By.NAME,"b") .send_keys("4")
-driver.find_element(By.)
+driver.find_element(By.NAME,"a") .send_keys("23")
+driver.find_element(By.NAME,"b") .send_keys("34")
+header_text = driver.find_element(By.XPATH,"//h1").text
+print(header_text)
+assert header_text=="Számológép"
+driver.find_element(By.ID,"submit").click()
+
+
+
+
