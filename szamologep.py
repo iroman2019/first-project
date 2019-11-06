3b0fcf2283d1c167f541dfff24e0da804d4e3b72
iroman2019 <roman.ildiko@pontsystems.eu>
2019-11-06 18:59:37 +0100

    Első autom commitom

--- szamologep.py
+++ szamologep.py
@@ -6,4 +6,4 @@ driver.get("https://iroman2019.github.io/first-project/")
 
 driver.find_element(By.NAME,"a") .send_keys("3")
 driver.find_element(By.NAME,"b") .send_keys("4")
-
+driver.find_element(By.)
